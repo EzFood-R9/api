3e6b4a3dd3521906519e53691810fdb57e1c549f
Tommi Kyllönen <98741756+t33k00@users.noreply.github.com>
2023-03-29 15:17:19 +0300

    Delete API/javaspring directory

--- API/javaspring/src/main/java/com/db/webapp/pwencoder/Passwordencoder.java
+++
@@ -1,7 +0,0 @@
-package com.db.webapp.pwencoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
-@Service
-public class Passwordencoder extends BCryptPasswordEncoder {
-}
